65d4ed0f7183fdb455c9040fdde810fcf97e27ef
minh18ndh <minh.nguyendohoang@gmail.com>
2024-10-05 14:31:45 +0700

    update baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://af7f-14-232-58-92.ngrok-free.app/',   // ngrok is used to expose the API endpoint
+    baseURL: 'https://04d0-14-232-134-77.ngrok-free.app/',   // ngrok is used to expose the API endpoint
     headers: {"ngrok-skip-browser-warning": "true"}   // Needed in order for our client HTTP requests not to be blocked by CORS
 });
